89f8ce2324ab3f2a89e8e3095f4668793c53936c
Jon Brighton <brightonsbox@hotmail.com>
2023-05-11 22:17:02 +0100

    Amending test to check that third party methods are called

--- src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -1,16 +1,24 @@
 package uk.gov.dwp.uc.pairtest;
 
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 public class TicketServiceImpl implements TicketService {
-    /**
-     * Should only have private methods other than the one below.
-     */
+
+    private final TicketPaymentService ticketPaymentService;
+    private final SeatReservationService seatReservationService;
+
+    public TicketServiceImpl(TicketPaymentService ticketPaymentService, SeatReservationService seatReservationService) {
+        this.ticketPaymentService = ticketPaymentService;
+        this.seatReservationService = seatReservationService;
+    }
 
     @Override
-    public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
-        throw new InvalidPurchaseException();
+    public void purchaseTickets(final Long accountId, final TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+        ticketPaymentService.makePayment(accountId, 10);
+        seatReservationService.reserveSeat(accountId, 1);
     }
 
 }
--- src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
+++ src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -1,17 +1,43 @@
 package uk.gov.dwp.uc.pairtest;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type.ADULT;
 
+@ExtendWith(MockitoExtension.class)
 class TicketServiceImplTest {
 
     private static final long VALID_ACCOUNT_ID = 123;
+    private static final TicketTypeRequest ONE_ADULT_TICKET = new TicketTypeRequest(ADULT, 1);
+    private static final int COST_FOR_ONE_ADULT = 10;
 
-    final TicketService service = new TicketServiceImpl();
+    @Mock
+    private TicketPaymentService ticketPaymentService;
+
+    @Mock
+    private SeatReservationService seatReservationService;
+
+    private TicketService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new TicketServiceImpl(ticketPaymentService, seatReservationService);
+    }
 
     @Test
     void purchaseTickets() {
-        service.purchaseTickets(VALID_ACCOUNT_ID);
+        service.purchaseTickets(VALID_ACCOUNT_ID, ONE_ADULT_TICKET);
+
+        verify(ticketPaymentService).makePayment(VALID_ACCOUNT_ID, COST_FOR_ONE_ADULT);
+        verify(seatReservationService).reserveSeat(VALID_ACCOUNT_ID, 1);
     }
 }
